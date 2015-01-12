de838f661aca1b1b55e6177ac38695a084ac2d08
Richard Farr <rfarr@oanda.com>
2015-01-12 16:14:45 -0500

    Add support for data section
        - Added new data function that takes std::string key/value
        - Data is built up in separate picojson::object and serialized into the
          top level object on destruction

--- LogDefer.h
+++ LogDefer.h
@@ -1,11 +1,14 @@
 #pragma once
 
+#include <functional>
+
 #include "picojson/picojson.h"
 
 class LogDefer {
   public:
     LogDefer(std::function<void (const std::string&)> callback);
     ~LogDefer();
+    void data(const std::string &key, const std::string &value);
     void add_log(int verbosity, const std::string &msg);
     void error(const std::string &msg);
     void warn(const std::string &msg);
@@ -16,6 +19,7 @@ class LogDefer {
     std::function<void (const std::string &)> callback_;
     struct timeval start_tv_;
     picojson::object o_;
+    picojson::object data_;
     picojson::array logs_;
     picojson::array timers_;
 };
